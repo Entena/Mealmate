d0a67360b845bc3d4b25116ac2a8e1408e7d530a
Cal <calientepermanente@gmail.com>
2015-04-28 14:59:12 -0400

    Added goals screen. Will add glue later.

---
+++ app/src/main/java/cs3724/group/mealmate/GoalsViewFragment.java
@@ -0,0 +1,30 @@
+package cs3724.group.mealmate;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class GoalsViewFragment extends Fragment {
+
+
+    public GoalsViewFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_goals_view, container, false);
+    }
+
+
+}
