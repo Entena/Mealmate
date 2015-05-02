52e926a76087b3cdfbb06cd9a145939c63beea6c
Pat <patsfan1@vt.edu>
2015-05-01 20:24:22 -0400

    Results are finally displaying nicely and the selection features are working. Note that we had to hack around the limitations of the standard adapter api and avoid memory recycling.

--- app/src/main/java/cs3724/group/mealmate/ScheduleMealCandidateFragment.java
+++ app/src/main/java/cs3724/group/mealmate/ScheduleMealCandidateFragment.java
@@ -1,40 +1,41 @@
 package cs3724.group.mealmate;
 
 
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.text.method.SingleLineTransformationMethod;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+import java.util.ArrayList;
+
+
 public class ScheduleMealCandidateFragment extends Fragment {
-
     public final static String FRAG_RETAIN_TAG = "FRAG_RETAIN";
     private final static int MAX_ENTRIES = 100;
-
+    private ArrayList<SearchResultsListItem> selectedMeals;
     // retained fragment
-    RetainedFragment retainFrag;
-
+    private RetainedFragment retainFrag;
     // DBs
-    DatabaseHandler userInfoDB;
-    SQLiteHelper foodDB;
-
+    private DatabaseHandler userInfoDB;
+    private SQLiteHelper foodDB;
     // UI Elements
-    TableLayout resultsTbl;
-
-
-    public ScheduleMealCandidateFragment() {
-        // Required empty public constructor
-    }
+    private ListView resultsTbl;
 
 
     @Override
@@ -42,61 +43,96 @@ public class ScheduleMealCandidateFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_schedule_meal_candidate, container, false);
-
-        retainFrag = (RetainedFragment) getFragmentManager()
-                .findFragmentByTag(FRAG_RETAIN_TAG);
-
-        // get UI elements
-        resultsTbl = (TableLayout) view.findViewById(R.id.resultsTable);
-
-        // get DBs
-        userInfoDB = retainFrag.getUserInfoDB();
-        foodDB = retainFrag.getFoodDB();
-
-        // get results
-        //MMResultSet rs = retainFrag.getLastResultSet();
-        //MMResultSet rs = foodDB.execQuery("SELECT * FROM foods WHERE CALS < 854 AND PROT > 26 AND FAT < 28 AND FIBER > 12 AND SOD < 657 AND DINING_HALL_ID LIKE '%Owens%'");
-        MMResultSet rs = foodDB.execQuery("SELECT * FROM foods WHERE CALS < 854 AND PROT > 26");
-
-        int count = 0;
-
-        while(rs.hasNext() && count < MAX_ENTRIES) {
-            TableRow tr = new TableRow(getActivity());
-            tr.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT));
-            CheckBox cb = new CheckBox(getActivity());
-            cb.setText("Add");
-            /*TextView foodName = new TextView(getActivity());
-            foodName.setText(rs.getFoodName());*/
-            TextView cals = new TextView(getActivity());
-            cals.setText(rs.getCalories());
-            TextView protein = new TextView(getActivity());
-            protein.setText(rs.getProtein());
-            TextView fat = new TextView(getActivity());
-            fat.setText(rs.getFat());
-            TextView fiber = new TextView(getActivity());
-            fiber.setText(rs.getFiber());
-            TextView sodium = new TextView(getActivity());
-            sodium.setText(rs.getSodium());
-            //TextView location = new TextView(getActivity());
-            //location.setText(rs.getDiningHall());
-
-            tr.addView(cb);
-            //tr.addView(location);
-            //tr.addView(foodName);
-            tr.addView(cals);
-            tr.addView(protein);
-            tr.addView(fat);
-            tr.addView(fiber);
-            tr.addView(sodium);
-
-            resultsTbl.addView(tr,
-                    new TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT,
-                            TableLayout.LayoutParams.WRAP_CONTENT));
-            rs.next();
-            count++;
-        }
+        setGlobals(view);
+        setListeners();
         return view;
     }
 
+    private void setGlobals(View view){
+        resultsTbl = (ListView) view.findViewById(R.id.resultsTable);
+        retainFrag = (RetainedFragment) getFragmentManager().findFragmentByTag(FRAG_RETAIN_TAG);
+        userInfoDB = retainFrag.getUserInfoDB();
+        foodDB = retainFrag.getFoodDB();
+        selectedMeals = new ArrayList<SearchResultsListItem>();
+    }
 
+    private void setListeners(){
+        //MMResultSet rs = retainFrag.getLastResultSet();
+        //MMResultSet rs = foodDB.execQuery("SELECT * FROM foods WHERE CALS < 854 AND PROT > 26 AND FAT < 28 AND FIBER > 12 AND SOD < 657 AND DINING_HALL_ID LIKE '%Owens%'");
+        MMResultSet rs = foodDB.execQuery("SELECT * FROM foods WHERE CALS < 854 AND PROT > 26");
+        int count = 0;
+        final ArrayList<SearchResultsListItem> food = new ArrayList<SearchResultsListItem>(MAX_ENTRIES);
+        while(rs.hasNext() && count < MAX_ENTRIES) {
+            SearchResultsListItem item = new SearchResultsListItem(rs.getFoodName(), rs.getDiningHall(), rs.getCalories(), rs.getProtein(), rs.getFat(), rs.getFiber(), rs.getSodium(), rs.getFoodID());
+            //food.add(rs.getFoodName()+"\n"+rs.getDiningHall()+"\n"+"Cal:"+rs.getCalories()+" P:"+rs.getProtein()+" Ft:"+rs.getFat()+" Fb:"+rs.getFiber()+" S:"+rs.getSodium());
+            food.add(item);
+            rs.next();
+            count++;
+        }
+        CustomAdapter adapter = new CustomAdapter(getActivity(), R.layout.ourlistitem, food);
+        resultsTbl.setAdapter(adapter);
+        resultsTbl.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        resultsTbl.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(selectedMeals.contains((SearchResultsListItem)parent.getAdapter().getItem(position))){
+                    selectedMeals.remove((SearchResultsListItem)parent.getAdapter().getItem(position));
+                    view.setBackgroundColor(view.getDrawingCacheBackgroundColor());
+                    ((CustomAdapter) parent.getAdapter()).removeSelected(position);
+                    Log.e("HIT", "VIEW REMOVING HIGH" + view.toString() + ":" + parent.getAdapter().getCount() + ":" + position);
+                    Log.e("HIT", "REMOVE HIGHLITE"+((SearchResultsListItem)parent.getAdapter().getItem(position)).name);
+                } else {
+                    ((CustomAdapter) parent.getAdapter()).setSelected(position);
+                    selectedMeals.add((SearchResultsListItem) parent.getAdapter().getItem(position));
+                    view.setBackgroundColor(getResources().getColor(R.color.highlighted_text_material_light));
+                    Log.e("HIT", "VIEW BEING SET WITH HIGH" + view.toString() + ":" + parent.getAdapter().getCount() + ":" + position);
+                    Log.e("HIT", "ADD THE LIST/HIGHLIGHT"+((SearchResultsListItem)parent.getAdapter().getItem(position)).name);
+                }
+            }
+        });
+    }
+
+    public class CustomAdapter extends ArrayAdapter<SearchResultsListItem> {
+        protected static final int NO_SELECTED_COLOR = 0xFFFF;
+        protected  final int SELECTED_COLOR = getResources().getColor(R.color.highlighted_text_material_light);
+        private ArrayList<SearchResultsListItem> items;
+        private LayoutInflater mInflater;
+        private int viewResourceId;
+        private ArrayList<Integer> selectedPositions;
+
+        public CustomAdapter(Activity activity,int resourceId, ArrayList<SearchResultsListItem> list) {
+            super(activity,resourceId,list);
+            // Sets the layout inflater
+            mInflater = (LayoutInflater)activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            // Set a copy of the layout to inflate
+            viewResourceId = resourceId;
+            // Set a copy of the list
+            items = list;
+            selectedPositions = new ArrayList<Integer>();
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            TextView tv = (TextView)convertView;
+            if (tv == null) {
+                tv = (TextView)mInflater.inflate(viewResourceId, null);
+            }
+            tv.setText(items.get(position).toString());
+            // Change the background color
+            if (selectedPositions.contains(position)){
+                tv.setBackgroundColor(SELECTED_COLOR);
+            } else {
+                tv.setBackgroundColor(NO_SELECTED_COLOR);
+            }
+            return tv;
+        }
+
+        public void setSelected(int position) {
+            selectedPositions.add(position);
+        }
+
+        public void removeSelected(int position){
+            selectedPositions.remove(new Integer(position));
+        }
+    }
 }
---
+++ app/src/main/java/cs3724/group/mealmate/SearchResultsListItem.java
@@ -0,0 +1,24 @@
+package cs3724.group.mealmate;
+
+public class SearchResultsListItem {
+    public String name, cal, protein, carbs, fat, fiber, sodium, diningHall, foodID;
+
+    public SearchResultsListItem(String name, String diningHall, String cal, String protein, String fat,
+                                 String fiber, String sodium, String foodID){
+        this.name = name; this.diningHall = diningHall; this.cal = cal; this.protein = protein;
+        this.fat = fat; this.fiber = fiber; this.sodium = sodium; this.foodID = foodID;
+    }
+
+    public String toString(){
+        return name+"\n"+diningHall+"\n"+"Cal:"+cal+" P:"+protein+" Ft:"+fat+" Fb:"+fiber+" S:"+sodium;
+    }
+
+    public boolean equals(Object obj){
+        if(obj instanceof SearchResultsListItem){
+            if(((SearchResultsListItem) obj).foodID.equals(foodID)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
